6fbe4e436af5251099b7f3e2b5f73006191dd950
nizamdiwan <52311458+nizamdiwan@users.noreply.github.com>
2022-11-27 14:30:32 -0500

    Landing page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import Header from './components/Header';
+import get_recommendation from './components/get_recommendation';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/crop4.jpg'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+  //git
+}));
+export default function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src="" className="App-logo" alt="logo" /> */}
-        <p>
-          Get Your Recommendation
-        </p>
-        <a>Make Effective Farming</a>
-      </header>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Header />
+      <get_recommendation />
     </div>
   );
 }
-
-export default App;
